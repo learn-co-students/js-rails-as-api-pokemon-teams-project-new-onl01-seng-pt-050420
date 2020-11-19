56a9f6bb124a43caf24a7265f96a4a79c117a49b
cedric-joaquin <joaquin.cedric@gmail.com>
2020-11-18 18:22:30 -0800

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,36 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetchTrainers()
+})
+
+function fetchTrainers() {
+    fetch(`${TRAINERS_URL}`)
+        .then(resp => resp.json())
+        .then(json => {
+            console.log(json)
+            let trainers = json['data']
+            trainers.forEach(trainer => {
+                createTrainerCard(trainer.attributes.name)
+                console.log(trainer.attributes.name)
+            })
+        })
+}
+
+function createTrainerCard(trainer) {
+    let div = document.createElement("div")
+    let main = document.querySelector("main")
+    let pTag = document.createElement("p")
+    div.className = "card"
+    pTag.innerHTML = trainer
+
+    let addPokemonBtn = document.createElement("button")
+    addPokemonBtn.innerHTML = "Add Pokemon"
+    div.appendChild(addPokemonBtn)
+
+    main.appendChild(div)
+}
+
+function fetchPokemon()
