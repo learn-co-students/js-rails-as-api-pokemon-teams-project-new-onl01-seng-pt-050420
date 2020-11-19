aecfa95f0c55b83c99c8bc452466a84411482cb8
wtwerner <wtwerner@gmail.com>
2020-11-18 21:56:06 -0600

    Add pokemons to cards

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -6,13 +6,22 @@ function createTrainerCard(trainer) {
     let div = document.createElement("div")
     let main = document.querySelector("main")
     let pTag = document.createElement("p")
+    let ul = document.createElement("ul")
     div.className = "card"
     pTag.innerHTML = trainer.attributes.name
+
+    let pokemons = trainer.attributes.pokemons
+    pokemons.forEach(pokemon => {
+        let li = document.createElement("li")
+        li.innerHTML = pokemon.attributes.nickname
+        ul.appendChild(li)
+    })
 
     let addPokemonBtn = document.createElement("button")
     addPokemonBtn.innerHTML = "Add Pokemon"
 
     div.appendChild(pTag)
+    div.appendChild(ul)
     div.appendChild(addPokemonBtn)
 
     main.appendChild(div)
@@ -27,7 +36,7 @@ function fetchTrainers() {
         let trainers = json['data']
         trainers.forEach(trainer => {
             createTrainerCard(trainer)
-            console.log(trainer.attributes.name)
+            console.log(trainer)
         })
     })
 }
