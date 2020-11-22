85fd3651c4f4066a979159d9693d329ad541d4ca
Catheryn Small <catheryn.small@gmail.com>
2020-11-22 17:13:54 -0500

    allow pokemons to show

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -1,2 +1,20 @@
 class PokemonsController < ApplicationController
+    def show
+        pokemon = Pokemon.find_by(id: params[:id])
+        render json: PokemonSerializer.new(pokemon)
+    end
+
+    def create
+        trainer = Trainer.find_by(id: pokemon_params[:trainer_id])
+        name = Faker::Name.first_name
+        species = Faker::Games::Pokemon.name
+        pokemon = Pokemon.create(nickname: name, species: species, trainer_id: trainer.id)
+        render json: pokemon
+    end
+
+    private
+
+    def pokemon_params
+        params.require(:pokemon).permit(:trainer_id)
+    end
 end
--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -2,6 +2,72 @@ const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
 
-function showTraiers(){
+const main = document.getElementById('container');
 
+function getTrainers(){
+    fetch(TRAINERS_URL)
+    .then(response => response.json())
+    .then(trainers => trainers.data.forEach(createTrainerCard));
 };
+
+function createTrainerCard(trainer){
+    const div = document.createElement('div');
+    const pTag = document.createElement('p');
+    const pokemons = trainer.relationships.pokemons.data;
+    const ul = document.createElement('ul');
+    const addButton = document.createElement('button');
+
+    addButton.innerText = "Add Pokemon"
+    addButton.addEventListener('click', createPokemon);
+    div.className = "card";
+    div.id = trainer.id;
+    pTag.innerText = trainer.attributes.name;
+
+    div.appendChild(pTag);
+    div.appendChild(addButton);
+    div.appendChild(ul);
+    main.append(div);
+
+    getPokemon(pokemons);
+};
+
+function getPokemon(pokemons){
+    for(const pokemon of pokemons){
+        let id = parseInt(pokemon.id)
+        let POKEMON_URL = `${POKEMONS_URL}/${id}`
+        fetch(POKEMON_URL)
+        .then(res => res.json())
+        .then(pokemon => addPokemon(pokemon));
+    }
+};
+
+function addPokemon(pokemon){
+    debugger
+    let trainer = parseInt(pokemon.data.relationships.trainer.data.id)
+    let div = document.getElementById(trainer);
+    let ul = div.querySelector('ul');
+    const li = document.createElement('li');
+    
+    li.innerText = pokemon.data.attributes.nickname;
+    ul.appendChild(li);
+};
+
+function createPokemon(event){
+    // debugger
+    if (event.target.nextElementSibling.childElementCount == 6){
+        alert("You already have 6 Pokemons")
+    } else {
+        let trainerId = event.path[1].id
+        fetch(POKEMONS_URL, {
+            method:"POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                trainer_id: trainerId
+            })
+        }).then(res => res.json()).then(pokemon => console.log(pokemon))
+        .catch(error => alert("Something went wrong"))};
+};
+getTrainers();
